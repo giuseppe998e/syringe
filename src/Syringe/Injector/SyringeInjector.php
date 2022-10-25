b4092dcb57558a811915feb3fd081550fc40d21f
Giuseppe Eletto <peppe.eletto@gmail.com>
2022-10-25 17:59:07 +0200

    refactor: minor code changes

--- src/Syringe/Injector/SyringeInjector.php
+++ src/Syringe/Injector/SyringeInjector.php
@@ -2,17 +2,21 @@
 
 namespace Syringe\Injector;
 
+use ReflectionMethod;
+
 interface SyringeInjector {
     /**
      * @param string $class
+     *
      * @return object
      */
     public function spawnClass(string $class): object;
 
     /**
-     * @param object $class
-     * @param \ReflectionMethod $method
+     * @param object           $class
+     * @param ReflectionMethod $method
+     *
      * @return mixed
      */
-    public function invokeMethod(object $class, \ReflectionMethod $method): mixed;
+    public function invokeMethod(object $class, ReflectionMethod $method): mixed;
 }
